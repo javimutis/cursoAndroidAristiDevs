de507bbaea28b88e9dfcef6ebef95df9060516dc
javimutis <javiera.mutis.frias@gmail.com>
2023-04-06 18:15:09 -0400

    Incluye nuevos ejercicios

--- app/src/main/java/com/cursoandroid/androidmaster/exercises/ExerciseLinearLayout2Activity.kt
+++ app/src/main/java/com/cursoandroid/androidmaster/exercises/ExerciseLinearLayout2Activity.kt
@@ -1,12 +1,12 @@
 package com.cursoandroid.androidmaster.exercises
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.cursoandroid.androidmaster.R
 
-class EjercicioFrameLayout : AppCompatActivity() {
+class ExerciseLinearLayout2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ejercicio_frame_layout)
+        setContentView(R.layout.activity_exercise_linear_layout2)
     }
-}
+    }
---
+++ app/src/main/java/com/cursoandroid/androidmaster/exercises/ExerciseLinearLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.cursoandroid.androidmaster.exercises
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cursoandroid.androidmaster.R
+
+class ExerciseLinearLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio_linear_layout)
+    }
+}
