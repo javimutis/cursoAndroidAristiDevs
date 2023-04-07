fa4aec9b4484def780de163782e5e2a9e42d43be
javimutis <javiera.mutis.frias@gmail.com>
2023-04-06 20:01:33 -0400

    Exercises Constraint Layout - Text and buttons - Chains - Guideline

---
+++ app/src/main/java/com/cursoandroid/androidmaster/exercises/ExerciseConstraintChainsActivity.kt
@@ -0,0 +1,12 @@
+package com.cursoandroid.androidmaster.exercises
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cursoandroid.androidmaster.R
+
+class ExerciseConstraintChainsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise_constraint_chains)
+    }
+}
